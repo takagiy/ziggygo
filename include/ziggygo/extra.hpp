597e8a8176f6f9a6e464d9be68921eca766aaf43
takagiy <takagiy.4dev@gmail.com>
2019-05-12 18:33:21 +0900

    initial commit

---
+++ include/ziggygo/extra.hpp
@@ -0,0 +1,1 @@
+#include "./extra/print_ascii.hpp"
---
+++ include/ziggygo/extra/print_ascii.hpp
@@ -0,0 +1,12 @@
+#ifndef COM_GITHUB_TAKAGIY_ZIGGYGO_EXTRA_PRINT_ASCII
+#define COM_GITHUB_TAKAGIY_ZIGGYGO_EXTRA_PRINT_ASCII
+
+#include <cstddef>
+
+namespace ziggygo {
+    namespace extra {
+        auto print_ascii(const map &map, std::size_t dots_per_char) {}
+    }
+}
+
+#endif
