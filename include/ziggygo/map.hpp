e3729bbbc039034a4e480c27d540666f85c82efd
takagiy <takagiy.4dev@gmail.com>
2019-05-17 12:44:59 +0900

    map knows only where the blocks locate, no cart size.

--- include/ziggygo/map.hpp
+++ include/ziggygo/map.hpp
@@ -5,32 +5,19 @@
 #include "./cart.hpp"
 #include "./path.hpp"
 #include <cstddef>
+#include <initializer_list>
 #include <vector>
 
 namespace ziggygo {
   template <std::size_t Width, std::size_t Height>
   class map {
-    std::vector<block> impassible_areas_;
+    std::vector<block> blocks_;
 
   public:
-    template <class... Blocks>
-    map(const cart &cart, const Blocks &... blocks)
-        : impassible_areas_{blocks...} {
-      auto padding_x = cart.width / 2;
-      auto padding_y = cart.height / 2;
-
-      for (auto &block : impassible_areas_) {
-        block.left -= padding_x;
-        block.right += padding_x;
-        block.top -= padding_y;
-        block.bottom += padding_y;
-      }
-    }
+    map(const std::initializer_list<block> &blocks) : blocks_{blocks} {}
 
     map(const map &) = default;
-
-    auto find_path() -> path { return path{}; }
-  };
+  }; // namespace ziggygo
 } // namespace ziggygo
 
 #endif
