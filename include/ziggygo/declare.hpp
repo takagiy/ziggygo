0c0cd9d6e6e16ac598e39f84eed6d984d41b52dc
takagiy <takagiy.4dev@gmail.com>
2019-05-18 10:25:41 +0900

    declare.hpp

--- include/ziggygo/declare.hpp
+++ include/ziggygo/declare.hpp
@@ -1,3 +1,6 @@
+#ifndef COM_GITHUB_TAKAGIY_ZIGGYGO_DECLARE
+#define COM_GITHUB_TAKAGIY_ZIGGYGO_DECLARE
+
 #include <cstddef>
 
 namespace ziggygo {
@@ -17,3 +20,5 @@ namespace ziggygo {
     template <std::size_t, std::size_t>
     class solver;
 };
+
+#endif
