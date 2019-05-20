04b5dbb36be56e304252d7ee1902ee52d5e99ca8
takagiy <takagiy.4dev@gmail.com>
2019-05-20 14:20:16 +0900

    improve visualizer

--- include/ziggygo/map.hpp
+++ include/ziggygo/map.hpp
@@ -22,7 +22,7 @@ namespace ziggygo {
 
     map(const map &) = default;
 
-    auto blocks() const -> const std::vector<block> & { return blocks_; }
+    auto get_blocks() const -> const std::vector<block> & { return blocks_; }
   };
 } // namespace ziggygo
 
